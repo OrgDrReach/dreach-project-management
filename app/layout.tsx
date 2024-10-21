61e383bd7dc3cf9f84c2ed854ac56b2346138ba1
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-10-21 14:08:01 +0530

    style 💎: Standardized webpage metadata
    - Added standardized webpage metadata such as title and description.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,32 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Metadata } from "next";
 
-const inter = Inter({ subsets: ["latin"] })
+export const metadata: Metadata = {
+	title: "Dr. Reach Insights",
+	description:
+		"A comprehensive project management tool that combines Kanban-based task management, RBAC, and AI-powered insights to streamline your workflow and boost productivity.",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
